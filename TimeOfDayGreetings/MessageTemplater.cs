fc73852c45e7ce549ca769e021efb84fa2e132f2
Simon Wendel <mail@simonwendel.se>
2024-10-03 12:00:40 +0200

    feat: add a template dictionary

---
+++ TimeOfDayGreetings/MessageTemplater.cs
@@ -0,0 +1,14 @@
+﻿namespace TimeOfDayGreetings;
+
+public class MessageTemplater : IMessageTemplater
+{
+    private static readonly Dictionary<TimeOfDay, string> Templates = new()
+    {
+        { TimeOfDay.Morning, "Good morning, {0}!" },
+        { TimeOfDay.Afternoon, "Good afternoon, {0}!" },
+        { TimeOfDay.Evening, "Good evening, {0}!" },
+        { TimeOfDay.Night, "Good night, {0}!" }
+    };
+
+    public string GetGreetingTemplate(TimeOfDay timeOfDay) => Templates[timeOfDay];
+}
---
+++ TimeOfDayGreetings/MessageTemplaterTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+
+namespace TimeOfDayGreetings;
+
+public class MessageTemplaterTests
+{
+    private readonly MessageTemplater sut = new();
+
+    [Fact]
+    public void GetGreetingTemplate_GivenMorning_ReturnsMorningTemplate() =>
+        sut.GetGreetingTemplate(TimeOfDay.Morning).Should().Be("Good morning, {0}!");
+
+    [Fact]
+    public void GetGreetingTemplate_GivenAfternoon_ReturnsAfternoonTemplate() =>
+        sut.GetGreetingTemplate(TimeOfDay.Afternoon).Should().Be("Good afternoon, {0}!");
+
+    [Fact]
+    public void GetGreetingTemplate_GivenEvening_ReturnsEveningTemplate() =>
+        sut.GetGreetingTemplate(TimeOfDay.Evening).Should().Be("Good evening, {0}!");
+
+    [Fact]
+    public void GetGreetingTemplate_GivenNight_ReturnsNightTemplate() =>
+        sut.GetGreetingTemplate(TimeOfDay.Night).Should().Be("Good night, {0}!");
+}

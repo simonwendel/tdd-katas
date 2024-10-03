c6e7f39985da0fff8c091b71b42c2922c24d35d5
Simon Wendel <mail@simonwendel.se>
2024-10-03 10:12:47 +0200

    feat: add TimeOfDay as actually discriminated union

---
+++ TimeDependent/TimeOfDay.cs
@@ -0,0 +1,31 @@
+﻿namespace TimeDependent;
+
+public sealed class TimeOfDay : IEquatable<TimeOfDay>
+{
+    private readonly TimeOfDayValue timeOfDayValue;
+
+    public static readonly TimeOfDay Morning = new(TimeOfDayValue.Morning);
+    public static readonly TimeOfDay Afternoon = new(TimeOfDayValue.Afternoon);
+    public static readonly TimeOfDay Evening = new(TimeOfDayValue.Evening);
+    public static readonly TimeOfDay Night = new(TimeOfDayValue.Night);
+
+    private enum TimeOfDayValue
+    {
+        Morning,
+        Afternoon,
+        Evening,
+        Night
+    }
+
+    private TimeOfDay(TimeOfDayValue timeOfDayValue) => this.timeOfDayValue = timeOfDayValue;
+
+    public bool Equals(TimeOfDay? other) => other != null && other.timeOfDayValue == timeOfDayValue;
+
+    public override bool Equals(object? obj) => ReferenceEquals(this, obj) || obj is TimeOfDay other && Equals(other);
+
+    public override int GetHashCode() => (int)timeOfDayValue;
+
+    public static bool operator ==(TimeOfDay? left, TimeOfDay? right) => Equals(left, right);
+
+    public static bool operator !=(TimeOfDay? left, TimeOfDay? right) => !Equals(left, right);
+}
---
+++ TimeDependent/TimeOfDayTests.cs
@@ -0,0 +1,82 @@
+﻿using FluentAssertions;
+
+namespace TimeDependent;
+
+public class TimeOfDayTests
+{
+    [Fact]
+    public void Equals_GivenSameTimeOfDay_ReturnsTrue()
+    {
+        TimeOfDay.Morning.Equals(TimeOfDay.Morning).Should().BeTrue();
+        TimeOfDay.Afternoon.Equals(TimeOfDay.Afternoon).Should().BeTrue();
+        TimeOfDay.Evening.Equals(TimeOfDay.Evening).Should().BeTrue();
+        TimeOfDay.Night.Equals(TimeOfDay.Night).Should().BeTrue();
+    }
+
+    [Fact]
+    public void EqualOperator_GivenSameTimeOfDay_ReturnsTrue()
+    {
+        // ReSharper disable once EqualExpressionComparison
+        (TimeOfDay.Morning == TimeOfDay.Morning).Should().BeTrue();
+
+        // ReSharper disable once EqualExpressionComparison
+        (TimeOfDay.Afternoon == TimeOfDay.Afternoon).Should().BeTrue();
+
+        // ReSharper disable once EqualExpressionComparison
+        (TimeOfDay.Evening == TimeOfDay.Evening).Should().BeTrue();
+
+        // ReSharper disable once EqualExpressionComparison
+        (TimeOfDay.Night == TimeOfDay.Night).Should().BeTrue();
+    }
+
+    [Fact]
+    public void NotEqualOperator_GivenSameTimeOfDay_ReturnsFalse()
+    {
+        // ReSharper disable once EqualExpressionComparison
+        (TimeOfDay.Morning != TimeOfDay.Morning).Should().BeFalse();
+
+        // ReSharper disable once EqualExpressionComparison
+        (TimeOfDay.Afternoon != TimeOfDay.Afternoon).Should().BeFalse();
+
+        // ReSharper disable once EqualExpressionComparison
+        (TimeOfDay.Evening != TimeOfDay.Evening).Should().BeFalse();
+
+        // ReSharper disable once EqualExpressionComparison
+        (TimeOfDay.Night != TimeOfDay.Night).Should().BeFalse();
+    }
+
+    [Fact]
+    public void Equals_GivenDifferentTimeOfDay_ReturnsFalse()
+    {
+        TimeOfDay.Morning.Equals(TimeOfDay.Afternoon).Should().BeFalse();
+        TimeOfDay.Morning.Equals(TimeOfDay.Evening).Should().BeFalse();
+        TimeOfDay.Morning.Equals(TimeOfDay.Night).Should().BeFalse();
+
+        TimeOfDay.Afternoon.Equals(TimeOfDay.Morning).Should().BeFalse();
+        TimeOfDay.Afternoon.Equals(TimeOfDay.Evening).Should().BeFalse();
+        TimeOfDay.Afternoon.Equals(TimeOfDay.Night).Should().BeFalse();
+
+        TimeOfDay.Evening.Equals(TimeOfDay.Morning).Should().BeFalse();
+        TimeOfDay.Evening.Equals(TimeOfDay.Afternoon).Should().BeFalse();
+        TimeOfDay.Evening.Equals(TimeOfDay.Night).Should().BeFalse();
+
+        TimeOfDay.Night.Equals(TimeOfDay.Morning).Should().BeFalse();
+        TimeOfDay.Night.Equals(TimeOfDay.Afternoon).Should().BeFalse();
+        TimeOfDay.Night.Equals(TimeOfDay.Evening).Should().BeFalse();
+    }
+
+    [Fact]
+    public void Equals_GivenObjectOfDifferentType_ReturnsFalse()
+    {
+        TimeOfDay.Morning.Equals(new object()).Should().BeFalse();
+    }
+
+    [Fact]
+    public void GetHashCode_GivenTimeOfDay_ReturnsTimeOfDayValueHashCode()
+    {
+        TimeOfDay.Morning.GetHashCode().Should().Be(0);
+        TimeOfDay.Afternoon.GetHashCode().Should().Be(1);
+        TimeOfDay.Evening.GetHashCode().Should().Be(2);
+        TimeOfDay.Night.GetHashCode().Should().Be(3);
+    }
+}

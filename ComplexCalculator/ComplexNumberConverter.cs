e39798a015cf779f7d49cb0027966cfeaff4b71b
Simon Wendel <mail@simonwendel.se>
2024-10-13 13:23:19 +0200

    feat: add converter from string to complex number

---
+++ ComplexCalculator/ComplexNumberConverter.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace ComplexCalculator;
+
+public partial class ComplexNumberConverter : IComplexNumberConverter
+{
+    public ComplexNumber FromString(string number)
+    {
+        ArgumentNullException.ThrowIfNull(number);
+        ArgumentException.ThrowIfNullOrWhiteSpace(number);
+
+        var regex = ComplexNumberRegex();
+        var matches = regex.Matches(number.Trim());
+        if (matches.Count != 1)
+        {
+            throw new ArgumentException(null, nameof(number));
+        }
+
+        var match = matches.First();
+
+        var reString = match.Groups["Re"].Value;
+        var re = reString switch
+        {
+            "" => 0,
+            _ => Convert.ToDouble(reString, CultureInfo.InvariantCulture)
+        };
+
+        var imString = match.Groups["Im"].Value;
+        var im = imString switch
+        {
+            "" => 0,
+            "i" => 1,
+            _ => Convert.ToDouble(imString.Replace("i", ""), CultureInfo.InvariantCulture)
+        };
+
+        return new ComplexNumber(re, im);
+    }
+
+    [GeneratedRegex(
+        @"^(?<Re>\d+\.?\d*)?\s*\+?\s*(?<Im>(?:\d+\.?\d*)?i)?$",
+        RegexOptions.Compiled | RegexOptions.Multiline | RegexOptions.RightToLeft)]
+    private static partial Regex ComplexNumberRegex();
+}
---
+++ ComplexCalculator/ComplexNumberConverterTests.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+
+namespace ComplexCalculator;
+
+public class ComplexNumberConverterTests
+{
+    private readonly ComplexNumberConverter sut = new();
+
+    [Fact]
+    public void FromString_GivenNullNumber_ThrowsException()
+    {
+        Action converting = () => sut.FromString(null!);
+        converting.Should().Throw<ArgumentNullException>().WithParameterName("number");
+    }
+
+    [Theory]
+    [InlineData("3 + 2.5i + 5 + 6i")]
+    [InlineData("3 + 2.5 + 6i")]
+    [InlineData("3 + 5i + 6i")]
+    [InlineData("3i + 2.5i")]
+    [InlineData("abc")]
+    [InlineData("3.6 ++ i2")]
+    [InlineData("3..6+i2")]
+    [InlineData("0.0.0+i2")]
+    [InlineData("0+2..0i")]
+    [InlineData("2.5ii")]
+    [InlineData("ii2.5")]
+    [InlineData($"\r\n")]
+    [InlineData($"\n")]
+    [InlineData("   ")]
+    [InlineData("")]
+    public void FromString_GivenInvalidStringRepresentationOfNumber_ThrowsException(string number)
+    {
+        Action converting = () => sut.FromString(number);
+#pragma warning disable CA1507
+        converting.Should().Throw<ArgumentException>().WithParameterName("number");
+#pragma warning restore CA1507
+    }
+
+    [Theory]
+    [InlineData("3 + 2.5i", 3, 2.5)]
+    [InlineData(" 3+2.5i", 3, 2.5)]
+    [InlineData("2.5i ", 0, 2.5)]
+    [InlineData("i", 0, 1)]
+    [InlineData(" 3", 3, 0)]
+    [InlineData("3 +   2.5i\r\n", 3, 2.5)]
+    [InlineData("3 + 2.5i\n", 3, 2.5)]
+    public void FromString_GivenValidStringRepresentationOfNumber_ReturnsNumber(string number, double re, double im)
+    {
+        sut.FromString(number).Should().Be(new ComplexNumber(re, im));
+    }
+}

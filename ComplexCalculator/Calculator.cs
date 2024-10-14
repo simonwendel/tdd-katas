d4cb5ddc59c67ff75baa8d901cfcaffd58cc4399
Simon Wendel <mail@simonwendel.se>
2024-10-14 08:03:15 +0200

    refactor: extract method common to add & sub

--- ComplexCalculator/Calculator.cs
+++ ComplexCalculator/Calculator.cs
@@ -8,35 +8,27 @@ public class Calculator(INumberStack stack, IComplexNumberConverter converter)
     private readonly INumberStack
         stack = stack ?? throw new ArgumentNullException(nameof(stack));
 
-    public void Add()
-    {
-        if (stack.Count < 2)
-        {
-            return;
-        }
-
-        var first = stack.Pop();
-        var second = stack.Pop();
-        stack.Push(first + second);
-    }
-
-    public void Sub()
-    {
-        if (stack.Count < 2)
-        {
-            return;
-        }
-
-        var first = stack.Pop();
-        var second = stack.Pop();
-        stack.Push(first - second);
-    }
-
     public void Enter(string number)
     {
         var complexNumber = converter.FromString(number);
         stack.Push(complexNumber);
     }
 
+    public void Add() => ApplyBinary((c1, c2) => c1 + c2);
+
+    public void Sub() => ApplyBinary((c1, c2) => c1 - c2);
+
     public object Display() => stack.ToString();
+
+    private void ApplyBinary(Func<ComplexNumber, ComplexNumber, ComplexNumber> operation)
+    {
+        if (stack.Count < 2)
+        {
+            return;
+        }
+
+        var first = stack.Pop();
+        var second = stack.Pop();
+        stack.Push(operation(first, second));
+    }
 }
